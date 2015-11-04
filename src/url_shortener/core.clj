2bf10da61445408d885f56f023270e6abeb8ecea
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2015-11-04 23:51:13 +0100

    Parallel load test

--- src/url_shortener/core.clj
+++ src/url_shortener/core.clj
@@ -3,6 +3,7 @@
   (:require [schema.core :as s]))
 
 (def ALPHABET "23456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ")
+(def CODE_LENGTH 6)
 
 (defn random-string
   [n]
@@ -17,10 +18,30 @@
   (let [result (ref nil)]
     (dosync
       (let [short (some #(when-not (contains? @database %) %)
-                        (repeatedly #(random-string 6)))]
+                        (repeatedly #(random-string CODE_LENGTH)))]
         (ref-set result short)
         (alter database assoc short url)))
     @result))
+
+#_(s/defn shorten-url-debug :- {:result     s/Str
+                                :collisions s/Int
+                                :attempts   s/Int}
+    [url :- s/Str]
+    (let [result (ref nil)
+          stats (atom {:collisions 0
+                       :attempts   0})]
+      (dosync
+        (let [[short collision-count] (loop [i 0]
+                                        (let [s (random-string CODE_LENGTH)]
+                                          (if (contains? @database s)
+                                            (recur (inc i))
+                                            [s i])))]
+          (ref-set result short)
+          (alter database assoc short url)
+          (swap! stats update-in [:collisions] + collision-count)
+          (swap! stats update-in [:attempts] inc)))
+      (merge {:result @result}
+             @stats)))
 
 (s/defn get-url :- (s/maybe s/Str)
   [short :- s/Str]
--- test/url_shortener/core_test.clj
+++ test/url_shortener/core_test.clj
@@ -1,5 +1,6 @@
 (ns url-shortener.core-test
   (:require [clojure.test :refer :all]
+            [clojure.pprint :refer [pprint]]
             [url-shortener.core :refer :all]))
 
 (defn stateful-mock-fn
@@ -9,6 +10,17 @@
   (let [results (atom (cons nil s))]
     (fn [& _]
       (first (swap! results next)))))
+
+(defn pow [n p]
+  (-> (partial * n)
+      (iterate 1)
+      (nth p)))
+
+(deftest can-pow
+  (are [r n p] (= r (pow n p))
+               1 2 0
+               4 2 2
+               27 3 3))
 
 (deftest can-stateful-mock-fn
   (let [f (stateful-mock-fn [1 2 3])]
@@ -30,6 +42,30 @@
       (is (= short2 "765432"))
       (is (= @database {"234567" "lalala"
                         "765432" "lalala"})))))
+
+(deftest can-shorten-url-concurrently
+  (let [old-ref-set ref-set]
+    (with-redefs [CODE_LENGTH 1
+                  database (ref {})
+                  ref-set (fn [r v]
+                            (Thread/sleep 1)
+                            (old-ref-set r v))]
+      (let [urls (map str (range (pow (count ALPHABET) CODE_LENGTH)))
+            shorts (pmap shorten-url urls)]
+        (is (= (count urls) (count (set shorts))))))))
+
+#_(deftest can-shorten-url-debug-concurrently
+    (let [old-ref-set ref-set]
+      (with-redefs [CODE_LENGTH 1
+                    database (ref {})
+                    ref-set (fn [r v]
+                              (Thread/sleep 1)
+                              (old-ref-set r v))]
+        (let [urls (map str (range (pow (count ALPHABET) CODE_LENGTH)))
+              shorts (pmap shorten-url-debug urls)]
+          (is (= (count urls) (count (set (map :result shorts)))))
+          (pprint {:top-attempts   (take 10 (sort-by :attempts > shorts))
+                   :top-collisions (take 10 (sort-by :collisions > shorts))})))))
 
 (deftest can-get-url
   (with-redefs [database (ref {"234567" "lalala"})]
